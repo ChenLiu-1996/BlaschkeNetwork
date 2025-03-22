c9bcdb01168045c9ae07d44b69036fe29d13b4c8
Chen Liu <chen.liu.cl2482@yale.edu>
2025-03-22 09:33:08 -0700

    commenting the analytical version

--- src/analytical/analytical_decomposition.py
+++ src/analytical/analytical_decomposition.py
@@ -4,62 +4,50 @@ import matplotlib.pyplot as plt
 from scipy.signal import hilbert
 
 
-def getBG(signal: np.ndarray,
-          fourier_poly_order, oversampling_rate, eps):
+def decompose_blaschke_products(high_freq_component: np.ndarray,
+                                fourier_poly_order: int,
+                                oversampling_rate: int,
+                                eps: float):
     '''
-    signal: the input signal. Assume shape (signal_len,)
+    high_freq_component: the high-frequency component of the signal. Assume shape (signal_len,)
     fourier_poly_order: the degree of Fourier polynomial. Used to truncated the functions B & G.
     oversampling_rate: oversampling rate
     eps: value of the threshold to cut the signal
     '''
-    # get the dimensions of the input signal
-    (num_channels, signal_len) = signal.shape
 
-    # get the index of the halfway point of the signal
+    num_channels, signal_len = high_freq_component.shape
     half_signal_len = signal_len // 2
+    time_idx = np.arange(1, oversampling_rate * signal_len + 1)
 
-    # create arrays for indexing
-    unit_m = np.arange(1, oversampling_rate * signal_len + 1)
-
-    # Plus oversampling by over
-    mask_N = (unit_m < (fourier_poly_order + 1))
-
-    signal_fft = np.fft.fft(signal)
-    f_fft_m = np.zeros((num_channels, signal_len * oversampling_rate), dtype=complex)
-    f_fft_m[:, 0:half_signal_len] = signal_fft[:, 0 :half_signal_len] * oversampling_rate
-
-    f_ana_m = np.fft.ifft(f_fft_m)
+    # Oversampling.
+    high_freq_fft = np.fft.fft(high_freq_component)
+    high_freq_fft_oversampled = np.zeros((num_channels, signal_len * oversampling_rate), dtype=complex)
+    high_freq_fft_oversampled[:, 0:half_signal_len] = high_freq_fft[:, 0 :half_signal_len] * oversampling_rate
+    high_freq_oversampled = np.fft.ifft(high_freq_fft_oversampled)
 
     # Step 2: evaluate G
     # compute log(abs(Hi)) of the analytic function Hi
-    f_abs_m = np.abs(f_ana_m)
-    if np.min(f_abs_m) < eps * np.max(f_abs_m):
-        # add eps to avoid division by zero
-        eps2 = (eps * np.max(f_abs_m))**2
-        log_abs_f = 0.5 * np.log(f_abs_m**2 + eps2)
+    signal_amplitude = np.abs(high_freq_oversampled)
+    if np.min(signal_amplitude) < eps * np.max(signal_amplitude):
+        # Avoid division by zero
+        eps2 = (eps * np.max(signal_amplitude))**2
+        log_abs_high_freq = 0.5 * np.log(signal_amplitude**2 + eps2)
     else:
-        eps2 = 0
-
-        log_abs_f = np.log(f_abs_m)
+        log_abs_high_freq = np.log(signal_amplitude)
 
     # Ana_log_abs_Hi = P_+(ln|Hi|)
-    m = np.mean(log_abs_f)
-    Ana_log_abs_F = m + 2 * np.fft.ifft(np.fft.fft(log_abs_f - m) * mask_N)
+    mean_signal = np.mean(log_abs_high_freq)
+    Ana_log_abs_F = mean_signal + 2 * np.fft.ifft(np.fft.fft(log_abs_high_freq - mean_signal) * (time_idx < (fourier_poly_order + 1)))
 
-    # filter the exp_plus
-    G_ana_m = np.fft.ifft(np.fft.fft(np.exp(Ana_log_abs_F)) * mask_N)
+    G_oversampled = np.fft.ifft(np.fft.fft(np.exp(Ana_log_abs_F)) * (time_idx < (fourier_poly_order + 1)))
+    blaschke_factor_oversampled = high_freq_oversampled / G_oversampled
 
-    # Step 3: evaluate B
-    B_ana_m = f_ana_m / G_ana_m
+    G = np.zeros((num_channels, signal_len), dtype = complex)
+    blaschke_factor = np.zeros((num_channels, signal_len), dtype = complex)
+    G[:, 0 : signal_len] = G_oversampled[:, 0 : signal_len * oversampling_rate :oversampling_rate]
+    blaschke_factor[:, 0 : signal_len] = blaschke_factor_oversampled[:, 0 : signal_len * oversampling_rate :oversampling_rate]
 
-    # Step 5: from signal oversample creates signal with original size
-    B_ana = np.zeros((num_channels, signal_len), dtype = complex)
-    G_ana = np.zeros((num_channels, signal_len), dtype = complex)
-
-    B_ana[:, 0 : signal_len] = B_ana_m[:, 0 : signal_len * oversampling_rate :oversampling_rate]
-    G_ana[:, 0 : signal_len] = G_ana_m[:, 0 : signal_len * oversampling_rate :oversampling_rate]
-
-    return B_ana, G_ana
+    return blaschke_factor, G
 
 
 def decompose_by_frequency(signal: np.ndarray,
@@ -75,37 +63,27 @@ def decompose_by_frequency(signal: np.ndarray,
     lowpass_order: the order of the low pass filter
     '''
 
-    l, ll = signal.shape
-    ll2 = ll // 2
-    unit_m = np.arange(oversampling_rate * ll) + 1
+    num_channels, signal_len = signal.shape
+    half_signal_len = signal_len // 2
+    time_idx = np.arange(oversampling_rate * signal_len) + 1
 
-    # Step 1: mask for hilbert transform with degre D, for low frequency part.
-    # we suppose that f is already analytic (support freq pos)
+    # Isolation of high and low frequency components.
+    mask_high_freq = (time_idx > lowpass_order) * (time_idx < (fourier_poly_order + 1))
+    mask_low_freq = (time_idx < (lowpass_order + 1))
 
-    # design the filter
+    signal_fft = np.fft.fft(signal, axis=1)
+    signal_fft_oversampled = np.zeros((num_channels, signal_len * oversampling_rate), dtype=complex)
+    signal_fft_oversampled[:, :half_signal_len] = signal_fft[:, :half_signal_len] * oversampling_rate
 
-    mask_N = (unit_m < (fourier_poly_order + 1))
-    maskLow = (unit_m < (lowpass_order + 1))
-    maskHigh = (unit_m > lowpass_order) * mask_N
+    high_freq_component_oversampled = np.fft.ifft(signal_fft_oversampled * mask_high_freq, axis=1)
+    low_freq_component_oversampled = np.fft.ifft(signal_fft_oversampled * mask_low_freq, axis=1)
 
-    # Filter in the fourier space with the 2 masks
-    # Plus oversampling by over
-    f_fft = np.fft.fft(signal, axis=1)
-    f_fft_m = np.zeros((l, ll * oversampling_rate), dtype=complex)
-    f_fft_m[:, :ll2] = f_fft[:, :ll2] * oversampling_rate
+    high_freq_component = np.zeros((num_channels, signal_len),dtype=complex)
+    low_freq_component = np.zeros((num_channels, signal_len), dtype=complex)
+    high_freq_component[:, :signal_len] = high_freq_component_oversampled[:, :signal_len*oversampling_rate:oversampling_rate]
+    low_freq_component[:, :signal_len] = low_freq_component_oversampled[:, :signal_len*oversampling_rate:oversampling_rate]
 
-    # Low_ana is low freq
-    Low_ana_m = np.fft.ifft(f_fft_m * maskLow, axis=1)
-    # High_ana is high freq
-    High_ana_m = np.fft.ifft(f_fft_m * maskHigh, axis=1)
-
-    High_ana = np.zeros((l, ll),dtype=complex)
-    Low_ana = np.zeros((l, ll), dtype=complex)
-
-    High_ana[:, :ll] = High_ana_m[:, :ll*oversampling_rate:oversampling_rate]
-    Low_ana[:, :ll] = Low_ana_m[:, :ll*oversampling_rate:oversampling_rate]
-
-    return High_ana, Low_ana
+    return high_freq_component, low_freq_component
 
 def blaschke_decomposition(signal: np.ndarray,
                            time: np.ndarray,
@@ -127,7 +105,7 @@ def blaschke_decomposition(signal: np.ndarray,
     NOTE: When lowpass_order = 1, `low_freq_component` at each iteration will correspond to `s_i`.
     `blaschke_product` at each iteration corresponds to `B_i`.
 
-    signal: the input signal. Assume shape (signal_len,)
+    signal: the input signal. Assume shape (signal_len,). CUrrently only support 1-channel signals.
     time: the input time. Assume shape (signal_len,)
     num_blaschke_iters: number of iterations of Blaschke decomposition. This gives (num_blaschke_iters - 1) Blaschke components.
     fourier_poly_order: the degree of Fourier polynomial. Used to truncated the functions B & G.
@@ -154,22 +132,23 @@ def blaschke_decomposition(signal: np.ndarray,
 
     # First iteration of decomposition.
     high_freq_component, low_freq_component = decompose_by_frequency(signal, fourier_poly_order, oversampling_rate, lowpass_order)
-    blaschke_product, G_ana = getBG(high_freq_component, fourier_poly_order, oversampling_rate, eps)
-    cumulative_blaschke_product = blaschke_product
+    curr_blaschke_factor, curr_G = decompose_blaschke_products(high_freq_component, fourier_poly_order, oversampling_rate, eps)
+    blaschke_product = curr_blaschke_factor
+    blaschke_factor = curr_blaschke_factor
 
     # All remaining iterations.
     for _ in range(1, num_blaschke_iters):
-        curr_high_freq_component, curr_low_freq_component = decompose_by_frequency(G_ana.reshape(1, -1), fourier_poly_order, oversampling_rate, lowpass_order)
-        curr_blaschke_product, G_ana = getBG(curr_high_freq_component, fourier_poly_order, oversampling_rate, eps)
+        curr_high_freq_component, curr_low_freq_component = decompose_by_frequency(curr_G.reshape(1, -1), fourier_poly_order, oversampling_rate, lowpass_order)
+        curr_blaschke_factor, curr_G = decompose_blaschke_products(curr_high_freq_component, fourier_poly_order, oversampling_rate, eps)
 
         low_freq_component = np.concatenate((low_freq_component, curr_low_freq_component), axis=0)
-        blaschke_product = np.concatenate((blaschke_product, curr_blaschke_product), axis=0)
-        cumulative_blaschke_product = np.concatenate((cumulative_blaschke_product, cumulative_blaschke_product[-1] * curr_blaschke_product), axis=0)
+        blaschke_factor = np.concatenate((blaschke_factor, curr_blaschke_factor), axis=0)
+        blaschke_product = np.concatenate((blaschke_product, blaschke_product[-1] * curr_blaschke_factor), axis=0)
 
     low_freq_component = low_freq_component[:, :int(low_freq_component.shape[1]/2)]
-    cumulative_blaschke_product = cumulative_blaschke_product[:, :int(cumulative_blaschke_product.shape[1]/2)] * np.tile(np.exp(-1j * 2 * np.pi * carrier_freq * time_arr), (num_blaschke_iters, 1))
+    blaschke_product = blaschke_product[:, :int(blaschke_product.shape[1]/2)] * np.tile(np.exp(-1j * 2 * np.pi * carrier_freq * time_arr), (num_blaschke_iters, 1))
 
-    return low_freq_component, blaschke_product, cumulative_blaschke_product
+    return low_freq_component, blaschke_factor, blaschke_product
 
 
 if __name__ == '__main__':
